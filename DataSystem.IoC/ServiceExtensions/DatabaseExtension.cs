604a2208be76e7758967ec447531b65a9f359a08
Carlos Eduardo Batista <carlosedbat@gmail.com>
2025-04-04 02:00:23 -0300

    feat:configura a camada de inversao de controle para a injecao de dependencias e configuracao do pattern extension com o automapper, database e DI extensions

---
+++ DataSystem.IoC/ServiceExtensions/DatabaseExtension.cs
@@ -0,0 +1,54 @@
+﻿using DataSystem.Infraestructure.Context;
+using DataSystem.Shared.Helpers.Environments;
+using DataSystem.Shared.Helpers.Log;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataSystem.IoC.ServiceExtensions
+{
+    public static class DatabaseExtension
+    {
+        public static string ConnectionString(IConfiguration iConfiguration)
+        {
+            EnvironmentVariablesDTO envVars = EnvironmentVariablesHelper.GetEnvironmentVariable();
+
+            string connectionString = iConfiguration.GetConnectionString("DefaultConnection");
+
+            return connectionString;
+        }
+
+        public static IServiceCollection AddHealthCheck(this IServiceCollection services, IConfiguration iConfiguration)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(ConnectionString(iConfiguration), name: "DataBaseSqlServer");
+
+            return services;
+        }
+
+        public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services, IConfiguration iConfiguration)
+        {
+            EnvironmentVariablesDTO envVars = EnvironmentVariablesHelper.GetEnvironmentVariable();
+            LogLevel logLevel = DatabaseLogLevelHelper.GetLogLevel(envVars.DatabaseLogLevel);
+
+            services.AddDbContext<AppDbContext>((serviceProvider, options) =>
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<AppDbContext>>();
+
+                options.UseSqlServer(ConnectionString(iConfiguration),
+                    sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(0))
+#if RELEASE
+                    .EnableSensitiveDataLogging(false)
+                    .EnableDetailedErrors(false)
+#else
+                    .EnableSensitiveDataLogging(true)
+                    .EnableDetailedErrors(true)
+#endif
+                    .LogTo(message => logger?.Log(logLevel, message), logLevel);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ DataSystem.IoC/ServiceExtensions/DependencyInjectionExtension.cs
@@ -0,0 +1,53 @@
+﻿using DataSystem.Application.Task.Service;
+using DataSystem.Domain.UnitOfWork;
+using DataSystem.Infraestructure.UnityOfWork;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace DataSystem.IoC.ServiceExtensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection ConfigureBaseDependency(this IServiceCollection services)
+        {
+            services.AddScoped<IWorkUnit, WorkUnit>();
+            services.AddScoped<ITaskServices, TaskServices>();
+            return services;
+        }
+        public static IServiceCollection ConfigureDependencyInjection(this IServiceCollection services)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                                 .Where(a => a.FullName.StartsWith("DataSystem"));
+
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes()
+                                    .Where(t => t.IsClass && t.IsPublic);
+
+                foreach (var type in types)
+                {
+                    var method = type.GetMethod("RegisterServicesInjection", BindingFlags.Public | BindingFlags.Static);
+
+                    if (method != null)
+                    {
+                        var parameters = method.GetParameters();
+                        if (parameters.Length == 1 && parameters[0].ParameterType == typeof(IServiceCollection))
+                        {
+                            try
+                            {
+                                method.Invoke(null, new object[] { services });
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine($"Error to register dependencies on class {type.FullName}: {ex.Message}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            return services;
+        }
+    }
+}
